3bad75881ba0b875957d06aa63e2bab415a68582
Akash V <akasha09960@gmail.com>
2025-10-24 21:51:29 +0530

    commit

---
+++ GCDOfNum/gcd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"Enter 2 numbers :";
+    cin>>n1>>n2;
+
+    while(n2!=0){
+        int temp=n2;
+        n2=n1%n2;
+        n1=temp;
+    }
+    cout<<"GCD :"<<n1;
+
+    return 0;
+}
